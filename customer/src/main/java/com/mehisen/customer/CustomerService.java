e9d2532a73c1da7b1b45a5d9300da0a28eea6edb
MahmoudMehisen <mahmoud.mehisen@gmail.com>
2023-07-19 14:22:21 +0300

    Change connection between servers to use discovery server not direct link

--- customer/src/main/java/com/mehisen/customer/CustomerService.java
+++ customer/src/main/java/com/mehisen/customer/CustomerService.java
@@ -22,7 +22,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse fraudCheckResponse =  restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class,customer.getId());
+                "http://FRAUD/api/v1/fraud-check/{customerId}", FraudCheckResponse.class,customer.getId());
         if(fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("fraudster");
         }
